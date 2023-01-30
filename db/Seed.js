7f35d8fea47c1a7742e0fd0ac38a7d206950dcab
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-30 13:23:50 -0600

    update

--- db/Seed.js
+++ db/Seed.js
@@ -4,6 +4,16 @@ const { client } = require('./index');
 
 // Page Imports
 
+
+    // Patient Imports
+
+    
+    // Staff Imports
+
+
+    // Treatment Imports
+
+
 // Step 2: User Methods
     // Method: Drop Tables
     async function dropTables(){
@@ -15,7 +25,7 @@ const { client } = require('./index');
             DROP TABLE IF EXISTS staff;
             DROP TABLE IF EXISTS treatment;
             `)
-        
+
             console.log("Finished dropping tables.")
         } catch(error){
             console.log("Error dropping tables!")
@@ -25,6 +35,41 @@ const { client } = require('./index');
 
 
     // Method: Create Tables:
+    // Method: createTables
+    async function createTables() {
+        try {
+            console.log('Starting to build tables...');
+            await client.query(`
+            CREATE TABLE user(
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(25) UNIQUE NOT NULL,
+                password VARCHAR(25) NOT NULL,
+                email VARCHAR(50) UNIQUE NOT NULL,
+                "is_active" BOOLEAN DEFAULT true
+            );
+            CREATE TABLE patient (
+                id SERIAL PRIMARY KEY,
+                first_name VARCHAR(25) NOT NULL,
+                last_name VARCHAR(25) NOT NULL,
+                date_of_birth DATE NOT NULL,
+                gender VARCHAR(10) NOT NULL,
+                address VARCHAR(100) NOT NULL,
+                phone_number VARCHAR(15) NOT NULL,
+                email VARCHAR(50) UNIQUE NOT NULL,
+                emergency_contact_name VARCHAR(50) NOT NULL,
+                emergency_contact_phone VARCHAR(15) NOT NULL,
+                "is_active" BOOLEAN DEFAULT true,
+                FOREIGN KEY (id) REFERENCES user(id)
+              );
+              
+
+            `);
+            console.log('Finished building tables.');
+            } catch (error) {
+            console.error('Error building tables!');
+            console.log(error);
+            }
+        };
 
 
     // Create Initials:
